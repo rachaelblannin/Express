6733191b9a8aacb09ed754e2ff4b8482e1e00470
rachaelblannin <rachblannin@gmail.com>
2022-10-06 16:14:08 +0100

    second commit

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 const express = require('express');
 const app = express();
-app.listen(4050);  
+
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     res.send("hello my name is Rach");
 });
 
-const server = app.listen(() =>{
-    console.log(`server started succesfully on port ${server.address().port}`);
-})
+
 
 let names = ["spongebob", "patrick", "squidward"];
 
 app.get("/getall", (req, res) => res.send(names)); 
 
+app.use((req, res, next) => {
+    console.log("request recieved at", new Date());
+    return next();
+})
+
 app.get("/get/:id", (req, res) => {
     const id = req.params.id;
-     res.send(names[id])});
-
+     res.send(names[id]);
+})
 app.get("/delete/:id", (req, res) => {
-    res.send(names.splice(req.params.id, 1))});
-    console.log(names);
+    res.send(names.splice(req.params.id, 1))
+});
+    
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
+const server = app.listen(1045,() =>{
+    console.log(`server started succesfully on port ${server.address().port}`);
+})
+
+
+
