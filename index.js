8cc93a1b56e2008200b2f60be56a6612a674b83b
rachaelblannin <rachblannin@gmail.com>
2022-10-07 10:48:04 +0100

    third commit

--- index.js
+++ index.js
@@ -20,14 +20,19 @@ app.use((req, res, next) => {
     return next();
 })
 
-app.get("/get/:id", (req, res) => {
+app.get("/get/:id", (req, res, next) => {
     const id = req.params.id;
+    if(id>=3)return next("id not found");
      res.send(names[id]);
 })
 app.get("/delete/:id", (req, res) => {
     res.send(names.splice(req.params.id, 1))
 });
     
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(400).send(err || "error");
+})
 
 const server = app.listen(1045,() =>{
     console.log(`server started succesfully on port ${server.address().port}`);
